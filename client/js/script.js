ebc8e800801a5c090ea3c12fb510d387bb7c27c7
MohaiminulEraj <eraj471@gmail.com>
2020-12-26 20:34:40 +0600

    html-css

---
+++ client/js/script.js
@@ -0,0 +1,8 @@
+function showPassBtn(){
+    var x = document.getElementById("myPassword");
+        if (x.type === "password") {
+        x.type = "text";
+        } else {
+        x.type = "password";
+        }
+}
